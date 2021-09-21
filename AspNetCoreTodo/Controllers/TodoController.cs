48f4b9384e6ee46f89df2f5a4222af2ef89c3130
Fernando de Francisco <f3rd3f@gmail.com>
2021-09-22 00:32:43 +0200

    Added Todo first MVC

---
+++ AspNetCoreTodo/Controllers/TodoController.cs
@@ -0,0 +1,32 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AspNetCoreTodo.Services;
+using AspNetCoreTodo.Models;
+
+namespace AspNetCoreTodo.Controllers
+{
+    public class TodoController : Controller
+    {
+        private readonly ITodoItemService _todoItemService;
+
+        public TodoController(ITodoItemService todoItemService)
+        {
+            _todoItemService = todoItemService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var items = await _todoItemService.GetIncompleteItemsAsync();
+
+            var model = new TodoViewModel()
+            {
+                Items = items
+            };
+
+            return View(model);
+            // Get tasks from DataBase
+            // Put tasks in a model
+            // Generate view using the model
+        }
+    }
+}
